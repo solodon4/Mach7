6ab1d5668be9659c004a6a14aa31adb94baec5d7
Yuriy Solodkyy <yuriy.solodkyy@gmail.com>
2014-09-05 01:11:03 -0700

    Added print visitor to CppCon presentation example

--- code/cppcon-visitors.cpp
+++ code/cppcon-visitors.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 struct VariableExp;
 struct Constant   ;
 struct NotExp     ;
@@ -6,29 +8,45 @@ struct OrExp      ;
 
 struct BooleanExpVisitor
 {
-    void visitVariableExp(const VariableExp&) {}
-    void visitConstant   (const Constant   &) {}
-    void visitNotExp     (const NotExp     &) {}
-    void visitAndExp     (const AndExp     &) {}
-    void visitOrExp      (const OrExp      &) {}
+    virtual void visitVariableExp(const VariableExp&) {}
+    virtual void visitConstant   (const Constant   &) {}
+    virtual void visitNotExp     (const NotExp     &) {}
+    virtual void visitAndExp     (const AndExp     &) {}
+    virtual void visitOrExp      (const OrExp      &) {}
 };
 
 struct BooleanExp               { virtual void accept(BooleanExpVisitor&) = 0;                             };
-struct VariableExp : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitVariableExp(*this); } std::string name; };
-struct Constant    : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitConstant   (*this); } bool value; };
-struct NotExp      : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitNotExp     (*this); } BooleanExp* operand; };
-struct AndExp      : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitAndExp     (*this); } BooleanExp* operand1; BooleanExp* operand2; };
-struct OrExp       : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitOrExp      (*this); } BooleanExp* operand1; BooleanExp* operand2; };
+struct VariableExp : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitVariableExp(*this); } VariableExp(const char* n) : name(n) {} std::string name; };
+struct Constant    : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitConstant   (*this); } Constant(bool b) : value(b) {}          bool value; };
+struct NotExp      : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitNotExp     (*this); } NotExp(BooleanExp* e) : operand(e) {}   BooleanExp* operand; };
+struct AndExp      : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitAndExp     (*this); } AndExp(BooleanExp* e1, BooleanExp* e2) : operand1(e1), operand2(e2) {} BooleanExp* operand1; BooleanExp* operand2; };
+struct OrExp       : BooleanExp { virtual void accept(BooleanExpVisitor& v) { v.visitOrExp      (*this); } OrExp (BooleanExp* e1, BooleanExp* e2) : operand1(e1), operand2(e2) {} BooleanExp* operand1; BooleanExp* operand2; };
+
+#include <iostream>
+
+void print(BooleanExp* exp)
+{
+    struct PrintVisitor : BooleanExpVisitor
+    {
+        void visitVariableExp(const VariableExp& x) { std::cout << x.name; }
+        void visitConstant   (const Constant   & x) { std::cout << x.value; }
+        void visitNotExp     (const NotExp     & x) { std::cout << '!'; print(x.operand); }
+        void visitAndExp     (const AndExp     & x) { print(x.operand1); std::cout << " & "; print(x.operand2); }
+        void visitOrExp      (const OrExp      & x) { print(x.operand1); std::cout << " | "; print(x.operand2); }
+    } printer;
+
+    exp->accept(printer);
+}
 
 #include <map>
 
 typedef std::map<std::string,bool> Context;
 
-bool evaluate(Context& ctx, BooleanExp& exp)
+bool evaluate(Context& ctx, BooleanExp* exp)
 {
     struct EvaluateVisitor : BooleanExpVisitor
     {
-        EvaluateVisitor(Context& c) : m_ctx(c) {}
+        EvaluateVisitor(Context& c) : m_ctx(c), result(false) {}
 
         bool     result;
         Context& m_ctx;
@@ -40,6 +58,19 @@ bool evaluate(Context& ctx, BooleanExp& exp)
         void visitOrExp      (const OrExp      & x) { result = evaluate(m_ctx, x.operand1) || evaluate(m_ctx, x.operand2); }
     } evaluator(ctx);
 
-    exp.accept(evaluator);
+    exp->accept(evaluator);
     return evaluator.result;
+}
+
+int main()
+{
+    BooleanExp* exp = new OrExp(new AndExp(new VariableExp("X"), new VariableExp("Y")), new NotExp(new VariableExp("Z")));
+
+    print(exp); std::cout << std::endl;
+
+    Context ctx;
+    ctx["X"] = !ctx["S"];
+
+    std::cout << evaluate(ctx, exp) << std::endl;
+    for (auto x : ctx) { std::cout << x.first << '=' << x.second << std::endl; }
 }
