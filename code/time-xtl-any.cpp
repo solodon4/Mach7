643eaaf23607644d1abcd42d52e87048249f3aab
Yuriy Solodkyy <yuriy.solodkyy@gmail.com>
2014-12-29 00:12:32 -0800

    Eliminated unused headers that got there due to copy-pasting

--- code/time-xtl-any.cpp
+++ code/time-xtl-any.cpp
@@ -43,14 +43,10 @@
 ///
 
 #include <iostream>
-#include "virpat.hpp"
 #include "testutils.hpp"
-
-#include "boost/variant.hpp"
-
 #include "type_switchN-patterns-xtl.hpp"
+#include "patterns/constructor.hpp"
 #include "adapt_boost_any.hpp"
-#include "patterns/all.hpp"
 
 struct P { int m_p; P(int i = 0) : m_p(i) {} };
 struct Q { int m_q; Q(int i = 0) : m_q(i) {} };
