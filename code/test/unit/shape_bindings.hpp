79506515abf29a72116f7980d152ce953c14b45f
Yuriy Solodkyy <yuriy.solodkyy@gmail.com>
2015-05-09 16:27:07 -0700

    CM to Members

--- code/test/unit/shape_bindings.hpp
+++ code/test/unit/shape_bindings.hpp
@@ -53,29 +53,29 @@ namespace mch ///< Mach7 library namespace
 {
 template <> struct bindings<Shape>    {};
 
-template <> struct bindings<Circle>   { CM(0,Circle::get_center); CM(1,Circle::radius); };
-template <> struct bindings<Square>   { CM(0,Square::upper_left); CM(1,Square::side);   };
-template <> struct bindings<Triangle> { CM(0,Triangle::first);    CM(1,Triangle::second); CM(2,Triangle::third); };
+template <> struct bindings<Circle>   { Members(Circle::get_center,Circle::radius); };
+template <> struct bindings<Square>   { Members(Square::upper_left,Square::side);   };
+template <> struct bindings<Triangle> { Members(Triangle::first,Triangle::second,Triangle::third); };
 
 // Members binding for std::pair and cloc C struct we use for unions case
 
 template <typename X, typename Y> 
-            struct bindings<std::pair<X,Y>> { CM(0,std::pair<X,Y>::first);  CM(1,std::pair<X,Y>::second); };
-template <> struct bindings<cloc>     { CM(0,cloc::first); CM(1,cloc::second); };
+            struct bindings<std::pair<X,Y>> { typedef std::pair<X,Y> arg_type; Members(arg_type::first,arg_type::second); };
+template <> struct bindings<cloc>     { Members(cloc::first,cloc::second); };
 
 // Members binding for discriminated union (ADT) case of Shape hierarchy
 
 template <> struct bindings<ADTShape>   { KS(ADTShape::kind); };
 
-template <> struct bindings<ADTShape,ADTShape::circle>     { KV(ADTShape,ADTShape::circle);   CM(0,ADTShape::center);     CM(1,ADTShape::radius); };
-template <> struct bindings<ADTShape,ADTShape::square>     { KV(ADTShape,ADTShape::square);   CM(0,ADTShape::upper_left); CM(1,ADTShape::size);   };
-template <> struct bindings<ADTShape,ADTShape::triangle>   { KV(ADTShape,ADTShape::triangle); CM(0,ADTShape::first);      CM(1,ADTShape::second); CM(2,ADTShape::third); };
+template <> struct bindings<ADTShape,ADTShape::circle>     { KV(ADTShape,ADTShape::circle);   Members(ADTShape::center,ADTShape::radius); };
+template <> struct bindings<ADTShape,ADTShape::square>     { KV(ADTShape,ADTShape::square);   Members(ADTShape::upper_left,ADTShape::size);   };
+template <> struct bindings<ADTShape,ADTShape::triangle>   { KV(ADTShape,ADTShape::triangle); Members(ADTShape::first,ADTShape::second,ADTShape::third); };
 
 // Members binding for inherited class from the above ADT
 
 template <> struct bindings<ADTShapeEx> { KS(ADTShape::kind); };
 
-template <> struct bindings<ADTShapeEx,ADTShape::circle>   { KV(ADTShape,ADTShape::circle);   CM(0,ADTShape::center);     CM(1,ADTShape::radius); CM(2,ADTShapeEx::extra_field); };
-template <> struct bindings<ADTShapeEx,ADTShape::square>   { KV(ADTShape,ADTShape::square);   CM(0,ADTShape::upper_left); CM(1,ADTShape::size);   CM(2,ADTShapeEx::extra_field); };
-template <> struct bindings<ADTShapeEx,ADTShape::triangle> { KV(ADTShape,ADTShape::triangle); CM(0,ADTShape::first);      CM(1,ADTShape::second); CM(2,ADTShape::third); CM(3,ADTShapeEx::extra_field); };
+template <> struct bindings<ADTShapeEx,ADTShape::circle>   { KV(ADTShape,ADTShape::circle);   Members(ADTShape::center,ADTShape::radius,ADTShapeEx::extra_field); };
+template <> struct bindings<ADTShapeEx,ADTShape::square>   { KV(ADTShape,ADTShape::square);   Members(ADTShape::upper_left,ADTShape::size,ADTShapeEx::extra_field); };
+template <> struct bindings<ADTShapeEx,ADTShape::triangle> { KV(ADTShape,ADTShape::triangle); Members(ADTShape::first,ADTShape::second,ADTShape::third,ADTShapeEx::extra_field); };
 } // of namespace mch
